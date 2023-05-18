6301899ad72010be633706e54ade52d08a11839b
Courtney Faulkner <courtneyfnfaulkner@gmail.com>
2023-05-18 17:18:10 -0500

    Removing generated stuff

--- src/components/TrainSchedule.js
+++ src/components/TrainSchedule.js
@@ -37,7 +37,7 @@ const TrainSchedule = () => {
     };
 
     const resetToCurrentTime = () => {
-        setNowTime(nowTime);
+        setNowTime(DateTime.local());
     };
 
     const getNextTrainSummary = () => {
